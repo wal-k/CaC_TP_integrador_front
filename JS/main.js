031e87bab3b66bd5f4de72d6df8cc187f3477ca7
wal-k <89323977+wal-k@users.noreply.github.com>
2023-10-06 12:51:25 -0300

    + carousel

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+console.log("script loaded");
+
+
+function enviar(){
+
+    let nombre = document.getElementById('campo_nombre').value;
+    let apellido = document.getElementById('campo_apellido').value;
+    alert("Tu mensaje ha sido enviado " + nombre + " " + apellido + "!");
+
+}
